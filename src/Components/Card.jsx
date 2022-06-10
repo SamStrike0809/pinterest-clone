252a285ea5af0b3d2502a4ba71bd4914dd9a19a1
Soumyadeep Mukherjee <1905359@kiit.ac.in>
2022-06-10 14:47:59 +0530

    test from gitpod

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "../styles.css";
 
 export default function Card({ post }) {
+  console.log("Changes using gitpod");
   return (
     <div>
       <img src={post.largeImageURL} />
