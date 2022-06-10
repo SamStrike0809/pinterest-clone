fa8915e3e537bf90413322a566eb84ca1e15a55c
Soumyadeep Mukherjee <1905359@kiit.ac.in>
2022-06-10 14:54:06 +0530

    using phone

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import "../styles.css";
 
 export default function Card({ post }) {
-  console.log("Changes using gitpod");
+  console.log("Changes using github");
+  console.log("changes from phon");
   return (
     <div>
       <img src={post.largeImageURL} />
